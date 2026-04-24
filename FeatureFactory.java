fd4ef423a9b9e329c45ce706777f686791fb3348
mohammad <mo7amadiama3el@gmail.com>
2026-04-24 09:49:43 +0300

    NER MEMM Assignment

---
+++ FeatureFactory.java
@@ -0,0 +1,246 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FeatureFactory {
+
+    /** Add any necessary initialization steps for your features here.
+     *  Using this constructor is optional. Depending on your
+     *  features, you may not need to intialize anything.
+     */
+    public FeatureFactory() {
+
+    }
+
+    /**
+     * Words is a list of the words in the entire corpus, previousLabel is the label
+     * for position-1 (or O if it's the start of a new sentence), and position
+     * is the word you are adding features for. PreviousLabel must be the
+     * only label that is visible to this method. 
+     */
+    private List<String> computeFeatures(List<String> words,
+					 String previousLabel, int position) {
+
+        List<String> features = new ArrayList<>();
+        String w = words.get(position);
+        features.add("w=" + w);
+        features.add("prevLabel=" + previousLabel);
+
+        // ===== Bias (مهم لرفع recall) =====
+        features.add("bias_PERSON_ٍSTRONG");
+
+        // ===== Prefix / Suffix =====
+        if (w.length() > 3) {
+            features.add("p3=" + w.substring(0,3));
+            features.add("s3=" + w.substring(w.length()-3));
+        }
+
+        // ===== Context =====
+        if (position > 0) {
+            String prev = words.get(position-1);
+            features.add("prev=" + prev);
+            features.add("prev_w=" + prev + "_" + w);
+        }
+
+        if (position < words.size()-1) {
+            String next = words.get(position+1);
+            features.add("next=" + next);
+        }
+        features.add("label_word=" + previousLabel + "_" + w);
+        // ===== احتمالية اسم (يزيد recall) =====
+        if (w.length() > 2) {
+            features.add("maybeName");
+        }
+
+        // ===== دعم أسماء عربية =====
+        if (w.equals("بن") || w.equals("ابن")) {
+            features.add("isBin");
+        }
+
+        if (position > 0 && position < words.size()-1) {
+            String prev = words.get(position-1);
+            String next = words.get(position+1);
+
+            if (w.equals("بن") || w.equals("ابن")) {
+                features.add("bin_context=" + prev + "_" + w + "_" + next);
+            }
+        }
+        if (previousLabel.equals("O") && w.length() > 2) {
+            features.add("O_to_PERSON_candidate");
+        }
+        if (!w.equals("في") && !w.equals("من") && !w.equals("على") && w.length() > 2) {
+            features.add("not_stopword_candidate");
+        }
+
+        // ===== Label interaction (قوي) =====
+        if (previousLabel.contains("PERSON")) {
+            features.add("continue_PERSON");
+        }
+        return features;
+    }
+
+
+    /** Do not modify this method **/    public List<Datum> readData(String filename) throws IOException {
+
+	List<Datum> data = new ArrayList<Datum>();
+	BufferedReader in = new BufferedReader(new FileReader(filename));
+
+	for (String line = in.readLine(); line != null; line = in.readLine()) {
+	    if (line.trim().length() == 0) {
+		continue;
+	    }
+	    String[] bits = line.split("\\s+");
+	    String word = bits[0];
+	    String label = bits[1];
+
+	    Datum datum = new Datum(word, label);
+	    data.add(datum);
+	}
+
+	return data;
+    }
+
+    /** Do not modify this method **/
+    public List<Datum> readTestData(String ch_aux) throws IOException {
+
+	List<Datum> data = new ArrayList<Datum>();
+
+	for (String line : ch_aux.split("\n")) {
+	    if (line.trim().length() == 0) {
+		continue;
+	    }
+	    String[] bits = line.split("\\s+");
+	    String word = bits[0];
+	    String label = bits[1];
+
+	    Datum datum = new Datum(word, label);
+	    data.add(datum);
+	}
+
+	return data;
+    }
+
+    /** Do not modify this method **/
+    public List<Datum> setFeaturesTrain(List<Datum> data) {
+	// this is so that the feature factory code doesn't accidentally use the
+	// true label info
+	List<Datum> newData = new ArrayList<Datum>();
+	List<String> words = new ArrayList<String>();
+
+	for (Datum datum : data) {
+	    words.add(datum.word);
+	}
+
+	String previousLabel = "O";
+	for (int i = 0; i < data.size(); i++) {
+	    Datum datum = data.get(i);
+
+	    Datum newDatum = new Datum(datum.word, datum.label);
+	    newDatum.features = computeFeatures(words, previousLabel, i);
+	    newDatum.previousLabel = previousLabel;
+	    newData.add(newDatum);
+
+	    previousLabel = datum.label;
+	}
+
+	return newData;
+    }
+
+    /** Do not modify this method **/
+    public List<Datum> setFeaturesTest(List<Datum> data) {
+	// this is so that the feature factory code doesn't accidentally use the
+	// true label info
+	List<Datum> newData = new ArrayList<Datum>();
+	List<String> words = new ArrayList<String>();
+	List<String> labels = new ArrayList<String>();
+	Map<String, Integer> labelIndex = new HashMap<String, Integer>();
+
+	for (Datum datum : data) {
+	    words.add(datum.word);
+	    if (labelIndex.containsKey(datum.label) == false) {
+		labelIndex.put(datum.label, labels.size());
+		labels.add(datum.label);
+	    }
+	}
+
+	// compute features for all possible previous labels in advance for
+	// Viterbi algorithm
+	for (int i = 0; i < data.size(); i++) {
+	    Datum datum = data.get(i);
+
+	    if (i == 0) {
+		String previousLabel = "O";
+		datum.features = computeFeatures(words, previousLabel, i);
+
+		Datum newDatum = new Datum(datum.word, datum.label);
+		newDatum.features = computeFeatures(words, previousLabel, i);
+		newDatum.previousLabel = previousLabel;
+		newData.add(newDatum);
+
+	    } else {
+		for (String previousLabel : labels) {
+		    datum.features = computeFeatures(words, previousLabel, i);
+
+		    Datum newDatum = new Datum(datum.word, datum.label);
+		    newDatum.features = computeFeatures(words, previousLabel, i);
+		    newDatum.previousLabel = previousLabel;
+		    newData.add(newDatum);
+		}
+	    }
+
+	}
+
+	return newData;
+    }
+
+    /** Do not modify this method **/
+    public void writeData(List<Datum> data, String filename)
+	throws IOException {
+
+
+	FileWriter file = new FileWriter(filename + ".json", false);
+
+	       
+	for (int i = 0; i < data.size(); i++) {
+	    try {
+		JSONObject obj = new JSONObject();
+		Datum datum = data.get(i);
+		obj.put("_label", datum.label);
+		obj.put("_word", base64encode(datum.word));
+		obj.put("_prevLabel", datum.previousLabel);
+
+		JSONObject featureObj = new JSONObject();
+
+		List<String> features = datum.features;
+		for (int j = 0; j < features.size(); j++) {
+		    String feature = features.get(j).toString();
+		    featureObj.put("_" + feature, feature);
+		}
+		obj.put("_features", featureObj);
+		obj.write(file);
+		file.append("\n");
+	    } catch (JSONException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+	}
+	file.close();
+    }
+
+    /** Do not modify this method **/
+    private String base64encode(String str) {
+	Base64 base = new Base64();
+	byte[] strBytes = str.getBytes();
+	byte[] encBytes = base.encode(strBytes);
+	String encoded = new String(encBytes);
+	return encoded;
+    }
+
+}
